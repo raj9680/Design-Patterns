afdf378f5b7ae441eb9d0bb574e9860b7f790e0a
raj kumar <raj.kumar@kochartech.corp>
2022-07-03 13:57:48 +0530

    neww

---
+++ src/Patterns/Unity_Strategy_Ioc_Srp_Soc/FactoryCustomer/Factory.cs
@@ -0,0 +1,45 @@
+﻿using MiddleLayer;
+using System;
+using System.Collections.Generic;
+
+namespace FactoryCustomer
+{
+    public static class Factory // Simple Factory pattern :- centralization of object creation
+    {
+        private static Dictionary<string, CustomerBase> custs = new Dictionary<string, CustomerBase>();  // xx
+        
+        // Moved this section to Create method to create the objects only when needed
+        
+        ///  static Factory()
+        ///  {
+        ///      custs.Add("Customer", new Customer());
+        ///      custs.Add("Lead", new Lead());
+        ///  }
+
+        public static CustomerBase Create(string TypeCust)
+        {
+            // Taken this section from WinFormCustomer project in order to achieve centralised object creation 1
+            // After our approach to remove multiple if else statements & it has been achieved by the creating a dictionary and assigning object cration as a value in constructor xx 2 & returning that in method
+            // But the problem in 2 is that object is getting created at runtime whether it is needed by application or not. To achieve this optimization we removed the ctor and moved that section inside create method based on requirement.3  XXX
+            ///  if(comboBox1.Text == "Customer")    xyz
+            ///  {
+            ///      cust = new Customer();
+            ///  }
+            ///  else
+            ///  {
+            ///     // lead = new Lead();    
+            ///      cust = new Lead();     1
+            ///  }
+
+            // Lazy load design pattern (load/create obj. when needed   opposite is Eager loading 3 - XXX
+            if (custs.Count == 0)
+            {
+                custs.Add("Customer", new Customer());
+                custs.Add("Lead", new Lead());
+            }
+
+            // RIP poly pattern -- Removing multiple if with polymorphism 2  xyz
+            return custs[TypeCust];
+        }
+    }
+}
---
+++ src/Patterns/Unity_Strategy_Ioc_Srp_Soc/WinFormsCustomer/FrmCustomer.cs
@@ -0,0 +1,97 @@
+﻿using FactoryCustomer;
+using MiddleLayer;
+using System;
+using System.Windows.Forms;
+
+namespace WinFormCustomer
+{
+    public partial class FrmCustomer : Form
+    {
+        // private Customer cust = null; // X
+        // private Lead lead = null;     // X
+
+        // Create one class reference instead of creating two class references   1
+        private CustomerBase cust= null;
+        public FrmCustomer()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+           // Moving that block to FactoryCustomer project to achieve centralised object creation  // 2
+           
+           // if(comboBox1.Text == "Customer")
+           // {
+           //     cust = new Customer();
+           // }
+           // else
+           // {
+           //     // lead = new Lead();    
+           //     cust = new Lead();     1
+           // }
+           
+            // creating object dynamically from FactoryCustomer project reference
+            cust = Factory.Create(comboBox1.Text);
+        }
+
+        private void SetCustomer()
+        {
+            cust.CustomerName = textBox2.Text;
+            cust.PhoneNumber = textBox4.Text;
+            cust.BillDate = textBox3.Text;
+            cust.BillAmount = Convert.ToDecimal(textBox1.Text);
+            cust.Address = richTextBox1.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /// X
+            //  if(comboBox1.Text == "Customer")
+            //  {
+            //     cust.Validate();
+            //  }
+            //  else
+            //  {
+            //     lead.Validate();
+            //  }
+
+            SetCustomer();
+            // Instead of X
+            cust.Validate();
+            
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
